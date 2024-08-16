645dc9b112b83b2c64d828f56b26235ebfa88a79
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2024-08-16 11:33:27 +0530

    Chapter 1 completed

---
+++ Chapter-1/03_data_types.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    //    int a;
+    //    a=8;
+
+    // int b=9;
+    // // float a=1.34;
+    // float a;
+    // a = 3.14;
+
+    // float a = 1234.13;
+    // float b = 123.323;
+
+    // char a = 'afsdlj';
+
+    return 0;
+}
