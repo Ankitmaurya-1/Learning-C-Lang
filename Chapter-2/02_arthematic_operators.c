6d25f80c2caaae44a4016c58a7538054170dd25a
Ankitmaurya-1 <ankitmaurya9560@gmail.com>
2024-08-23 18:04:48 +0530

    Chapter 2 50% Done

---
+++ Chapter-2/02_arthematic_operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 20;
+    int c = a + b;
+    printf("The sum of a %d and b %d is:%d", a, b, c);
+    return 0;
+}
--- Chatper-0/01_first.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main (){
-    printf("Hello world this is ankit");
-    return 0;
-
-}
